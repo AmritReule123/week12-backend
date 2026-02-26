dc8431d16ccefb71683bc5957083236987e93fc3
Amrit Reule <reuleamrit47@gmail.com>
2026-02-26 18:10:04 +0545

    week12

--- app.js
+++ app.js
@@ -12,31 +12,6 @@ require("./database/connection")
 app.use(express.json());
 
 
-// // ✅ GET all users
-// app.get("/users", async (req, res) => {
-//   const datas = await User.findAll();
-//   res.json({
-//     message: "Users fetched successfully",
-//     datas
-//   });
-// });
-
-
-// // ✅ POST new user
-// app.post("/users", async (req, res) => {
-//   const { userName, email, password, age } = req.body;
-
-//   const newUser = await User.create({
-//     userName,
-//     email,
-//     password,
-//     age
-//   });
-  
-//  res.json({
-//   message: "user automatically changed"
-//  })
-// })
 
 // localhost:4949/api + /hello = localhost:4949/api/hello
 /// localhost:4949/api + /books/:id = localhost:4949/api/books/:id
